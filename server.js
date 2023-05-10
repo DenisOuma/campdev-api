ebc57cf625fd1121880402ad699f94f1a097fa47
Denis Ouma <dennisouma38@gmail.com>
2023-05-10 18:35:29 +0300

    Setting up the routes for the application requests

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Working with routes for the application
+
+app.get("/api/v1/bootcamps", (req, res) => {
+	res.status(200).json({ success: "true", msg: "Show all bootcamps" });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+	res.status(200).json({ success: "true", msg: "Add new Bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+	res
+		.status(200)
+		.json({ success: "true", msg: `Update BootCamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+	res
+		.status(200)
+		.json({ success: "true", msg: `Delete BootCamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 // run server
