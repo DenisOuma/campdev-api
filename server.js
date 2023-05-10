8658aa95776569ca54ad856b8fc715e62f462e22
Denis Ouma <dennisouma38@gmail.com>
2023-05-10 18:49:58 +0300

    Setting up the routs section for the application

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const bootcamps = require("./routes/bootcamps");
 
+const dotenv = require("dotenv");
+// Rote files
 // Loading the process or config file
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-// Working with routes for the application
-
-app.get("/api/v1/bootcamps", (req, res) => {
-	res.status(200).json({ success: "true", msg: "Show all bootcamps" });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-	res.status(200).json({ success: "true", msg: "Add new Bootcamp" });
-});
-
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-	res
-		.status(200)
-		.json({ success: "true", msg: `Update BootCamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-	res
-		.status(200)
-		.json({ success: "true", msg: `Delete BootCamp ${req.params.id}` });
-});
+// Moute routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
