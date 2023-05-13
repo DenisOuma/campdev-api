0efff0480138db23b8e24bcd2fbb097e7a44c0ed
Denis Ouma <dennisouma38@gmail.com>
2023-05-13 19:34:57 +0300

    conncecting to data base

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 const express = require("express");
-const bootcamps = require("./routes/bootcamps");
-
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+
+const connectDB = require("./config/db");
 // Rote files
+
 // Loading the process or config file
 dotenv.config({ path: "./config/config.env" });
 
+// connect to database
+connectDB();
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 
 // Moute routers
 app.use("/api/v1/bootcamps", bootcamps);
 
+// set Port application
 const PORT = process.env.PORT || 5000;
 
 // run server
-app.listen(
+const server = app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
+	)
 );
+
+process.on("unhandledRejection", (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+
+	// cloese Server and exit process
+	server.close(() => process.exit(1));
+});
