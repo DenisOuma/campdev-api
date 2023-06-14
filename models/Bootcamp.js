b1cae527dbce04631caf7bdb55224cdeee71b6c6
Denis Ouma <dennisouma38@gmail.com>
2023-06-14 23:26:18 +0300

    application slug generate

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const slugify = require("slugify");
 const BootcampSchema = new mongoose.Schema({
 	name: {
 		type: String,
@@ -99,4 +99,10 @@ const BootcampSchema = new mongoose.Schema({
 	},
 });
 
+// create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+	this.slug = slugify(this.name, { lower: true });
+	console.log("Slugify ran", this.name);
+	next();
+});
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
