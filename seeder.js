60b5102b067bc3a0ab785b487f69efc999359ee3
Denis Ouma <dennisouma38@gmail.com>
2023-06-15 16:16:34 +0300

    Adding an application seeder for inserting data and removing data if needed

---
+++ seeder.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load the env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Load the models
+const Bootcamp = require("./models/Bootcamp");
+
+// connect to my mongo dataBase
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+// Reading the json file
+
+const bootcamps = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+// Import to database
+
+const importData = async () => {
+	try {
+		await Bootcamp.create(bootcamps);
+		console.log("Data Imported....".green.inverse);
+		process.exit();
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+// Deletye inserted Data
+const deleteData = async () => {
+	try {
+		await Bootcamp.deleteMany();
+		console.log("Data Destroyed....".red.inverse);
+		process.exit();
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+if (process.argv[2] === "-i") {
+	importData();
+} else if (process.argv[2] === "-d") {
+	deleteData();
+}
